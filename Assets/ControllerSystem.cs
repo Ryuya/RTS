e53c4bb3919990cdc383937b2dc48b4ff0fe152e
Ryuya <main.higashi.pg@gmail.com>
2021-02-27 08:55:14 +0900

    クリック位置にキャラクターが走る処理

---
+++ Assets/ControllerSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerSystem : MonoBehaviour
+{
+    private PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo = new RaycastHit();
+            float maxDistance = 100f;
+            bool isHit = Physics.Raycast(ray,out hitInfo , maxDistance);
+            if (isHit)
+            {
+                player.RUN(hitInfo.point);
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum State
+{
+    WAIT,
+    HOLD,
+    RUN,
+    ATTACK,
+}
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    public Vector3 targetPositon;
+
+    public State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.WAIT:
+                _agent.SetDestination(transform.position);
+                break;
+            case State.HOLD:
+                break;
+            case State.RUN:
+                if (targetPositon != null)
+                {
+                    _agent.SetDestination(targetPositon);
+                }
+                break;
+            case State.ATTACK:
+                
+                break;
+        }
+    }
+
+    public void RUN(Vector3 targetPosition)
+    {
+        state = State.RUN;
+        this.targetPositon = targetPosition;
+    }
+}
